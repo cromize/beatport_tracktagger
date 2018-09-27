f2fe6e553f8c710c6d9d802d7635e9838864b6e1
cromize <mikezemanek@gmail.com>
2018-09-27 14:28:51 +0200

    input args works, but fails when no input is supplied

--- to_flac.sh
+++ to_flac.sh
@@ -1,14 +1,82 @@
 #!/bin/bash
 
-# TODO: add output folder arg
 shopt -s globstar
 
-echo "Tool for converting wav, aiff files to flac (using sox)"
+die() {
+  printf '%s\n' "$1" >&2
+  exit 1
+}
+
+# input args
+input_folder=
+output_folder=
+while :; do
+  case $1 in
+    # --input
+    -i|--input)
+      if [ "$2" ]; then
+        input_folder=("$(readlink -nf $2)"/**/*.{wav,aiff})
+        shift
+      fi
+      ;;
+
+    # --output
+    -o|--output)
+      if [ "$2" ]; then
+        output_folder="$(readlink -nf $2)"
+        if [ ! -d "$output_folder" ]; then
+          # folder doesn't exist
+          mkdir "$output_folder"
+        fi
+        shift
+      else
+        output_folder={.}.flac
+      fi
+      ;;
+    *)
+      break
+  esac
+  shift
+done
+
+echo "tool for converting wav, aiff files to flac (using sox)"
+echo "by cromize"
 echo
 
-audio_files=(**/*.{wav,aiff})
-audio_files_count="${#audio_files[@]}"
-export audio_files_count
+audio_files_count="${#input_folder[@]}"
 
-parallel -k --env audio_files_count 'sox {} {.}.flac | echo "{#}/$audio_files_count) {}"' ::: **/*.{wav,aiff}
+export audio_files_count
+export input_folder
+export output_folder
+
+echo "files: $audio_files_count"
+
+# input arg
+if [ ! $input_folder ]; then
+  command='sox {}' 
+else
+  command='sox '${input_folder}
+fi
+
+# output arg
+if [ ! $output_folder ]; then
+  command+=' {.}.flac'
+else
+  command+=' '$output_folder/{/.}.flac
+fi
+
+# print iteration info
+# ********
+command+=' | echo "{#}/$audio_files_count)" '
+
+if [ ! $output_folder ]; then
+  command+='{}'
+else
+  command+=$output_folder/{/.}.flac
+fi
+# ********
+
+# TODO: BUG: it fails, when input_folder array is empty
+# run in parallel
+parallel -k -j30 --env audio_files_count,output_folder,input_folder $command ::: ${input_folder[@]} 
 
