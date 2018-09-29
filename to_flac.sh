c51932adfd6b2722c0a3f0abfc76f31e910d47f8
cromize <mikezemanek@gmail.com>
2018-09-29 14:12:43 +0200

    works with whitespace path now

--- to_flac.sh
+++ to_flac.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 shopt -s globstar
-
 die() {
   printf '%s\n' "$1" >&2
   exit 1
@@ -15,7 +14,7 @@ while :; do
     # --input
     -i|--input)
       if [ "$2" ]; then
-        input_folder=("$(readlink -nf $2)"/**/*.{wav,aiff})
+        input_folder=("$(readlink -nf $2)")
         shift
       fi
       ;;
@@ -43,7 +42,8 @@ echo "tool for converting wav, aiff files to flac (using sox)"
 echo "by cromize"
 echo
 
-audio_files_count="${#input_folder[@]}"
+files="$input_folder"/**/*.{wav,aiff}
+audio_files_count="${#files[@]}"
 
 export audio_files_count
 export input_folder
@@ -52,31 +52,34 @@ export output_folder
 echo "files: $audio_files_count"
 
 # input arg
-if [ ! $input_folder ]; then
+if [ ! "$input_folder" ]; then
   command='sox {}' 
 else
-  command='sox '${input_folder}
+  command='sox "'"$input_folder"/{/}'"'
 fi
 
 # output arg
-if [ ! $output_folder ]; then
+if [ ! "$output_folder" ]; then
   command+=' {.}.flac'
 else
-  command+=' '$output_folder/{/.}.flac
+  command+=' "'"$output_folder"/{/.}.flac'"'
 fi
 
 # print iteration info
 # ********
 command+=' | echo "{#}/$audio_files_count)" '
 
-if [ ! $output_folder ]; then
+if [ ! "$output_folder" ]; then
   command+='{}'
 else
-  command+=$output_folder/{/.}.flac
+  command+="$output_folder"/{/.}.flac
 fi
 # ********
 
+echo $command
+
+# TODO: make skip system
 # TODO: BUG: it fails, when input_folder array is empty
 # run in parallel
-parallel -k -j30 --env audio_files_count,output_folder,input_folder $command ::: ${input_folder[@]} 
+parallel -k -j30 --env audio_files_count,output_folder,input_folder $command ::: "${input_folder[@]}"/**/*.{wav,aiff}
 
