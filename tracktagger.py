f22a1fd48b1933462972ebeed682f99675244898
cromize <mikezemanek@gmail.com>
2019-03-25 18:00:11 +0100

    filepaths are handled properly now by python's Path class, fix artwork clear bug

--- tracktagger.py
+++ tracktagger.py
@@ -16,6 +16,7 @@ from mutagen.id3 import ID3
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import Picture, FLAC
 from os.path import isfile
+from pathlib import Path
 
 num_worker_threads=20
 
@@ -45,19 +46,17 @@ class Track:
     self.genre = ''
     self.label = ''
     self.file_name = ''
-    self.file_type = ''
 
   def queryPage(self):
     try:
       page = requests.get('https://www.beatport.com/track/aa/' + self.beatport_id)
-    except requests.exceptions.RequestException as e:
+    except Exception as e:
       print(f"Error cannot get track info!")
       sys.exit(1)
     return html.fromstring(page.content)
 
   def getTags(self):
     tree = self.queryPage()
-
     artistCount = tree.xpath('//*[@id="pjax-inner-wrapper"]/section/main/div[2]/div/div[1]/span[2]/a')
     artistCount = len(artistCount)
 
@@ -79,10 +78,9 @@ class Track:
 
   def printTrackInfo(self):
     print ('Track: ', end='')
-
     x = 1
     for artist in self.artists:
-      print (artist, end='')
+      print(artist, end='')
       if len(self.artists) > 1 and x < len(self.artists):
         print (', ', end='')
       x += 1
@@ -98,15 +96,19 @@ class Track:
            f'Beatport ID: {self.beatport_id}\n')
     print ('')
     
-  def openDatabaseJSON(file):
-    with open(file, 'r') as file:
-      Track.json_database = file.readlines()
+  def openDatabaseJSON(src):
+    with open(src, 'r') as f:
+      Track.json_database = f.readlines()
 
-  def saveDatabaseJSON(file):
-    with open(file, 'w') as file:
+  def saveDatabaseJSON(src):
+    with open(src, 'w') as f:
       for k, v in Track.database.items():
-        track_json = json.dumps(v.__dict__)
-        file.write(track_json + '\n')
+        import copy
+        vv = copy.copy(v) 
+        if "file_path" in vv.__dict__:
+          del vv.__dict__["file_path"]
+        track_json = json.dumps(vv.__dict__)
+        f.write(track_json + '\n')
 
   def loadTracks():
     for track in Track.json_database:
@@ -114,27 +116,24 @@ class Track:
       track_object.__dict__ = json.loads(track)
       Track.database[track_object.beatport_id] = track_object
 
-  def scanFiles(input):
+  def scanFiles(src):
     filetypes = '.flac', '.mp3'
-    files = glob.glob(os.path.join(input, '**'), recursive=True)
     outputFiles = []
+    # single file
+    if Path(src).is_file():
+      files = [Path(src)]
+    else:
+      files = Path(src).glob('**/*')
 
-    # For flacs, mp3s 
+    # if suffix matches filetype
     for f in files:
-      # win
-      if os.name == 'nt':
-        filename = f.split('\\')[-1]
-      # linux 
-      else:
-        filename = f.split('/')[-1]
-
-      # if ends with correct filetype
-      for filetype in filetypes:
-        if filename.lower().endswith(filetype):
-          if beatport_id_pattern.match(filename):
-            outputFiles.append(f)
-            Track.track_count += 1
-            break
+      if Path(f).suffix in filetypes: 
+        if beatport_id_pattern.match(Path(f).name):
+          beatport_id = beatport_id_pattern.match(Path(f).name).group()[:-1]
+          if beatport_id in Track.database.keys():
+            Track.database[beatport_id].file_path = Path(f)
+          outputFiles.append(Path(f))
+          Track.track_count += 1
     return outputFiles
 
   def trackInDB(beatport_id):
@@ -144,23 +143,20 @@ class Track:
 
   def fileTagsUpdate(self):
     path = self.file_path
-    if self.file_type == ".flac":
+    if Path(self.file_name).suffix == ".flac":
       audiof = FLAC(path)  
-    elif self.file_type == ".mp3":
+    elif Path(self.file_name).suffix == ".mp3":
       audiof = EasyID3(path)  
 
     # single artist
     if len(self.artists) == 1: audiof['ARTIST'] = self.artists
+    # multiple artists
     else:
       temp = ""
-      count = 0
-
-      # multiple artists
-      for artist in self.artists:
+      for i, artist in enumerate(self.artists):
         temp += artist
-        if count < len(self.artists) - 1:
+        if i < len(self.artists) - 1:
            temp += ", "
-        count += 1
       audiof['ARTIST'] = [temp]
 
     audiof['DATE'] = self.released[:4]
@@ -173,31 +169,30 @@ class Track:
   # query and save artwork
   # artwork(500x500)
   def saveArtwork(self):
-    if self.file_type == ".flac":
-      audiof = FLAC(self.file_path)  
-    elif self.file_type == ".mp3":
-      audiof = ID3(self.file_path)  
     img = requests.get(self.artwork_url).content
-    audiof.add(mutagen.id3.APIC(3, 'image/jpeg', 3, 'Front cover', img))
+    if Path(self.file_name).suffix == ".flac":
+      audiof = FLAC(self.file_path)  
+      img = Picture()
+      img.type = 3
+      img.desc = 'artwork'
+      img.data = requests.get(self.artwork_url).content
+      audiof.add_picture(img)
+    elif Path(self.file_name).suffix == ".mp3":
+      audiof = ID3(self.file_path)  
+      audiof.add(mutagen.id3.APIC(3, 'image/jpeg', 3, 'Front cover', img))
     audiof.save()
 
   def cleanTags(filepath):
-    if filepath.endswith(".flac"):
+    if Path(filepath).suffix == ".flac":
       audiof = FLAC(filepath)  
-    elif filepath.endswith(".mp3"):
-      audiof = ID3(filepath)  
-    audiof.delete()
+      audiof.clear_pictures()
+    elif Path(filepath).suffix == ".mp3":
+      audiof = EasyID3(filepath)  
+      audiof.delete()
     audiof.save()
 
   def addTrackToDatabase(filepath):
-    # win
-    if os.name == 'nt':
-      filename = filepath.split('\\')[-1]
-    # linux 
-    else:
-      filename = filepath.split('/')[-1]
-
-    print("omg:", filepath)
+    filename = Path(filepath).name
 
     # if is valid beatport file
     if beatport_id_pattern.match(filename):
@@ -210,14 +205,8 @@ class Track:
 
       # create and get tags
       track = Track(beatport_id)
-      track.file_path = os.path.abspath(filepath)
+      track.file_path = str(Path(filepath))
       track.file_name = filename
-
-      # fill filetype
-      if track.file_name.lower().endswith(".flac"):
-        track.file_type = ".flac"
-      elif track.file_name.lower().endswith(".mp3"):
-        track.file_type = ".mp3"
 
       # try 10 times
       for tries in range(10):
@@ -289,10 +278,6 @@ if __name__ == "__main__":
     input_parser.print_help()  
     sys.exit(0)
 
-  # scan for flac files
-  beatport_id_pattern = re.compile('^[0-9]+[_]')
-  flac_files = Track.scanFiles(args.input)
-
   # load existing db
   if isfile(args.load_db):
     print('Database found! Loading data...')
@@ -300,32 +285,40 @@ if __name__ == "__main__":
     Track.loadTracks()
     print(f'Number of tracks in db: {len(Track.database)}' )
 
+  # scan for flac files
+  beatport_id_pattern = re.compile('^[0-9]+[_]')
+  work_files = Track.scanFiles(args.input)
+
   # get tags from beatport
   if args.sync:
-    Track.processFiles(flac_files)
+    print('\nGetting tags from beatport')
+    Track.processFiles(work_files)
     Track.saveDatabaseJSON('tracks.db')
 
   # tag audio files
   if args.tag_files: 
-    print('Updating audio tags...')
+    print('\nUpdating audio tags...')
     i = 1
     for k, v in Track.database.items():
-      print(f'{i}/{Track.track_count} - {v.file_name}')
-      v.fileTagsUpdate()
-      i += 1
+      # for scanned files only
+      if "file_path" in v.__dict__:
+        print(f'{i}/{Track.track_count} - {v.file_name}')
+        v.fileTagsUpdate()
+        i += 1
 
   # clean file tags
   if args.clean_tags:
-    print("Clearing tags")
-    for idx, file in enumerate(flac_files):
-      print(f'{idx+1}/{len(flac_files)} - {file}')
-      Track.cleanTags(file)
+    print("\nClearing tags")
+    for idx, f in enumerate(work_files):
+      print(f'{idx+1}/{len(work_files)} - {f}')
+      Track.cleanTags(f)
 
   # save artwork
   if args.artwork:
-    print("Saving artwork")
+    print("\nSaving artwork")
     for idx, (k, v) in enumerate(Track.database.items()):
-      print(f'{idx+1}/{Track.track_count} - {v.file_name}')
-      v.saveArtwork()
+      if "file_path" in v.__dict__:
+        print(f'{idx+1}/{Track.track_count} - {v.file_name}')
+        v.saveArtwork()
 
-  print('Done')
+  print('All done')
